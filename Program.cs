76b02c490f12a252add89f8bb0c9bcdbb0191e1a
DanielGG1618 <57752868+DanielGG1618@users.noreply.github.com>
2020-10-19 20:26:18 +0400

    321.1

--- Program.cs
+++ Program.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.Common;
+using MySql.Data.MySqlClient;
 
 namespace Formula1
 {
     static class Program
     {
+        public static MySqlConnection Connection;
+
         /// <summary>
         /// Главная точка входа для приложения.
         /// </summary>
@@ -16,7 +18,37 @@ namespace Formula1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+
+            string connString = "Server=VH287.spaceweb.ru;" +
+                ";Database= beavisabra_cars" +
+                ";port=3306;User Id=beavisabra_cars;password=Beavis1989";
+
+            Connection = new MySqlConnection(connString);
+            Connection.Open();
             Application.Run(new Form1());
+            Connection.Close();
+
+        }
+
+        /// <summary>
+        /// Select-запрос
+        /// </summary>
+        public static List<string> Select(string Text)
+        {
+            //Результат
+            List<string> results = new List<string>();
+            //Создать команду
+            MySqlCommand command = new MySqlCommand(Text, Connection);
+            //Выполнить команду
+            DbDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+                for (int i = 0; i < reader.FieldCount; i++)
+                    results.Add(reader.GetValue(i).ToString());
+
+            reader.Close();
+
+            return results;
         }
     }
 }
